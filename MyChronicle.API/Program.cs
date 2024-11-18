47281cf486d45723498ef65236a8b43f83f66801
Karolina <k4aro0@gmail.com>
2024-11-18 21:44:15 +0100

    fix: database creds

--- MyChronicle.API/Program.cs
+++ MyChronicle.API/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
         policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:7072");
     });
 });
